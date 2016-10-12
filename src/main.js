dd202acf62eda124e6c69ae77b05e6564ba2c62c
Michael Ang <angmerica@gmail.com>
2016-10-12 02:27:19 -0400

    add some jquery magic

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-require('./app/app.module');
-
 import './styles.css';
 
-console.log('Hello World, Loading main');
+$('.scroll-link').click(function (event) {
+    var anchor = $(this).attr('href');
+
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( anchor ).offset().top
+    }, 500);
+});
+
+$('.sc-btn-header, .sc-btn-redefine').click(function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( '#services' ).offset().top
+    }, 500);
+});
