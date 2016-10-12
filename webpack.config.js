7771ed9797903b8cc8ee7dd7870f6eba1c1cc941
Michael Ang <angmerica@gmail.com>
2016-10-12 01:56:41 -0400

    remove production and integration builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 switch (process.env.NODE_ENV) {
     case 'production':
         console.log('===Running production===');
-        module.exports = require('./config/webpack.production.js');
         break;
     case 'integration':
         console.log('===Running integration===');
-        module.exports = require('./config/webpack.integration.js');
         break;
     case 'development':
     default:
