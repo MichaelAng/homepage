6a2728c58029196bf3c07cb7bce0b586c7b16bbd
Michael Ang <angmerica@gmail.com>
2016-10-14 08:44:58 -0400

    change to es6 syntax

--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
 import './styles.css';
 
-$('.scroll-link').click(function (event) {
-    var anchor = $(this).attr('href');
-
+$('.scroll-link').click((event) => {
+    var anchor = $(event.currentTarget).attr('href');
     event.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $( anchor ).offset().top
-    }, 500);
+    $('html, body').animate({ scrollTop: $( anchor ).offset().top }, 500);
 });
 
-$('.sc-btn-header, .sc-btn-redefine').click(function (event) {
+$('.sc-btn-header, .sc-btn-redefine').click((event) => {
     event.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $( '#services' ).offset().top
-    }, 500);
+    $('html, body').animate({ scrollTop: $( '#services' ).offset().top }, 500);
 });
